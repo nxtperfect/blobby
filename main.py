083b5de59579f8384728d1dceddc935c943c94fb
NxtPerfect <alakaxan@protonmail.com>
2025-08-21 08:17:59 +0200

    feat: move bot to separate class

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ from os import getenv
 from dotenv import load_dotenv
 import discord
 
-load_dotenv()
-_TOKEN = getenv("DISCORD_TOKEN")
 
-client = discord.Client(intents=discord.Intents.default())
+class Blobby(discord.Client):
+    _TOKEN: str | None
+
+    def __init__(self, intents=discord.Intents.default()):
+        load_dotenv()
+        self._TOKEN = getenv("DISCORD_TOKEN")
+        super().__init__(intents=intents)
+
+    async def on_ready(self):
+        print(f"{super().user} connected!")
+
+    def run(self):
+        super().run(self._TOKEN)
 
 
-@client.event
-async def on_ready():
-    print(f"{client.user} connected!")
-    for guild in client.guilds:
-        print(f"Connected to {guild.name}")
-
-
-client.run(_TOKEN)
+# client = discord.Client(intents=discord.Intents.default())
 
 
 def main():
     print("Hello from blobby!")
+    client = Blobby()
     client.run()
 
 
